f23c22fe46de4306aa8242b441d2ab3960ffe44a
dmachard <5562930+dmachard@users.noreply.github.com>
2022-08-27 07:06:59 +0200

    add timeout to http requests

--- publicaddr/akamai.py
+++ publicaddr/akamai.py
@@ -3,6 +3,8 @@ import requests
 
 NAME = "Akamai"
 
+timeout = 5
+
 http_servers = {
     "ip4": "http://ipv4.whatismyip.akamai.com/",
     "ip6": "http://ipv6.whatismyip.akamai.com/",
@@ -12,9 +14,9 @@ def lookup(ipversion, ipproto):
     ret = None
     try:
         if ipversion == 4:
-            ret = requests.get(http_servers["ip4"]).text.rstrip()
+            ret = requests.get(http_servers["ip4"], timeout=timeout).text.rstrip()
         if ipversion == 6:
-            ret = requests.get(http_servers["ip6"]).text.rstrip()
+            ret = requests.get(http_servers["ip6"], timeout=timeout).text.rstrip()
     except requests.exceptions.RequestException:
         pass
     return ret
