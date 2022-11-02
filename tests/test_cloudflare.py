84738cf9b43bc0ecd166b24f0b0b8d523d744955
dmachard <5562930+dmachard@users.noreply.github.com>
2022-11-02 09:30:11 +0100

    cloudflare testing

--- tests/test_cloudflare.py
+++ tests/test_cloudflare.py
@@ -1,11 +1,19 @@
 import unittest
 import publicaddr
 class TestCloudflare(unittest.TestCase):
-    def test_get_ip4(self):
+    def test_get_ip4_dns(self):
         """get dns ip4"""
         ip = publicaddr.get(provider=publicaddr.CLOUDFLARE, ip=publicaddr.IPv4)
         self.assertNotEqual(ip, "")
-    def test_get_ip6(self):
+    def test_get_ip6_dns(self):
         """get dns ip6"""
         ip = publicaddr.get(provider=publicaddr.CLOUDFLARE, ip=publicaddr.IPv6)
+        self.assertNotEqual(ip, "")
+    def test_get_ip4_http(self):
+        """get http ip4"""
+        ip = publicaddr.get(provider=publicaddr.CLOUDFLARE, ip=publicaddr.IPv4, proto=publicaddr.HTTPS)
+        self.assertNotEqual(ip, "")
+    def test_get_ip6_http(self):
+        """get http ip6"""
+        ip = publicaddr.get(provider=publicaddr.CLOUDFLARE, ip=publicaddr.IPv6, proto=publicaddr.HTTPS)
         self.assertNotEqual(ip, "")
